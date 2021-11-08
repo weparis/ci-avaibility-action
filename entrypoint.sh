e59ba603f6189db70e96140ba31884b6f1611488
Pr. X <xavier.delestre.pro@gmail.com>
2021-11-08 21:35:48 +0100

    init ci-avaibility action

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ function exportBranchSlug {
 
 function peek {
   if [ -z ${INPUT_INFRA_IDS} ]; then echo "Missing INFRA_IDS. Did you specify 'with' action parameter ?"; fi
-  curl -sS -o /dev/null "https://europe-west1-ci-availability.cloudfunctions.net/setInfraForProject?projectName=${GITHUB_REPOSITORY}&infraIds=${BRANCH_SLUG}"
+  curl -sS -o /dev/null "https://europe-west1-ci-availability.cloudfunctions.net/setInfraForProject?projectName=${GITHUB_REPOSITORY}&infraIds=${INPUT_INFRA_IDS}"
   INFRA_ID=$(curl -sS "https://europe-west1-ci-availability.cloudfunctions.net/peekAvailableInfra?projectName=${GITHUB_REPOSITORY}&branchId=${BRANCH_SLUG}")
   echo "INFRA_ID=$INFRA_ID" >> $GITHUB_ENV
   echo Picked $INFRA_ID
